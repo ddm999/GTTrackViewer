78e106747b050d54d383c2749c6177b3974666a2
Nenkai <neenkaai@gmail.com>
2021-11-20 03:21:52 +0100

    Add comment about Atan2RadToDeg

--- GTTrackEditor/Utils/MathUtils.cs
+++ GTTrackEditor/Utils/MathUtils.cs
@@ -4,6 +4,11 @@ namespace GTTrackEditor.Utils
 {
     public class MathUtils
     {
+        /// <summary>
+        /// Converts (-π to π) to degrees. 
+        /// </summary>
+        /// <param name="rad"></param>
+        /// <returns></returns>
         public static float Atan2RadToDeg(float rad)
         {
             float degrees = (rad * 180) / MathF.PI;
